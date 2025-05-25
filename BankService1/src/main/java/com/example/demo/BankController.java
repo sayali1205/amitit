28cb2a67302e6994c5adbf11ccef6c9117775f3d
sayali <sayalikawarkar@gmail.com>
2025-05-25 22:01:11 +0530

    added bank method

--- BankService1/src/main/java/com/example/demo/BankController.java
+++ BankService1/src/main/java/com/example/demo/BankController.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BankController {
+	@GetMapping("/bank")
+	public String getBankDetails(String type) {
+		
+		return "SBI";
+	}
 
 }
